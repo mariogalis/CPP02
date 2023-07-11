783aa90cfa983fe7eb5718bbcb7c1ed77a5bd5d4
mariogalis <magonzal@student.42madrid.com>
2023-07-11 19:56:23 +0200

    alan

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: magonzal <magonzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 17:50:24 by magonzal          #+#    #+#             */
+/*   Updated: 2023/07/11 19:31:35 by magonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+class Fixed
+{
+	public:
+
+		Fixed(void);
+		Fixed(const Fixed &copy);
+		~Fixed();
+		Fixed &operator=(const Fixed &copy);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+	
+	private:
+
+		int	_whole;
+		static int const _bits = 8;
+};
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: magonzal <magonzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 17:50:26 by magonzal          #+#    #+#             */
+/*   Updated: 2023/07/11 17:51:12 by magonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int	main( void )
+{
+Fixed a;
+Fixed b( a );
+Fixed c;
+c = b;
+std::cout << a.getRawBits() << std::endl;
+std::cout << b.getRawBits() << std::endl;
+std::cout << c.getRawBits() << std::endl;
+return 0;
+}
